2687d48d098e4eefe99b0876574cc3244318b0bd
Groot-yu <1175970209@qq.com>
2019-11-04 14:40:02 +0800

    me

--- app/src/main/java/com/yyg/thefirstlineofcode/MainActivity.java
+++ app/src/main/java/com/yyg/thefirstlineofcode/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] data = {"开始启程——你的第一行Android代码", "先从看得到的入手——探究活动", "软件也要拼脸蛋——UI开发的点点滴滴", "手机平板要兼顾——探究碎片"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
